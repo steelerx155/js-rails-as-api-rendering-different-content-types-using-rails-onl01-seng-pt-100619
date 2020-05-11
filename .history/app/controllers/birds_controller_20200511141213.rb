a371d01a39cd1978045ba6f214b5542f8913c55c
steelerx155 <steelerx155@gmail.com>
2020-05-11 15:14:02 -0400

    Done.

--- .history/app/controllers/birds_controller_20200511141213.rb
+++ .history/app/controllers/birds_controller_20200511141213.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    render plain: "Hello #{@birds[3].name}"
   end
 end
